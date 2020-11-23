cb7b6863819df18acd648758ec0ada21a715e042
Alexander Apostolov <alex21ander08@gmail.com>
2020-11-23 21:09:03 +0100

    pipeline structure minimal

---
+++ src/pipeline.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 20:23:24 2020
+
+@author: Alexander
+"""
+
+def get_model():
+    raise NotImplementedError
+    
+def get_train_test_loaders():
+    raise NotImplementedError
+    
+def metric(prediction, labels):
+    return iou(prediction, labels)
+
+
+
+def iou(prediction, labels, smooth = 1e-6):
+    """
+    Intersection over union of two boxes
+
+    Parameters
+    ----------
+    prediction : torch.Tensor
+        Labels of the batch.
+    labels : torch.Tensor
+        Labels of the batch.
+    smooth : float, optional
+        Smoothing factor to avoid 0 division. The default is 1e-6.
+
+    Returns
+    -------
+    float
+        mean iou over the batch.
+
+    """
+    
+    # You can comment out this line if you are passing tensors of equal shape
+    # But if you are passing output from UNet or something it will most probably
+    # be with the BATCH x 1 x H x W shape
+    outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
+    
+    intersection = (prediction & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
+    union = (prediction | labels).float().sum((1, 2))         # Will be zero if both are 0
+    
+    iou = (intersection + smooth) / (union + smooth)  # We smooth our devision to avoid 0/0
+    
+    #thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
+    
+    return iou.mean()  # Or iou if you are interested in each element of the batch
+
+def train(model, criterion, dataset_train, dataset_test, optimizer, num_epochs, device):
+    """
+    Parameters
+    ----------
+    model : torch.nn.Module
+        DESCRIPTION.
+    criterion : torch.nn.modules.loss._Loss
+        DESCRIPTION.
+    dataset_train : torch.utils.data.DataLoader
+        DESCRIPTION.
+    dataset_test : torch.utils.data.DataLoader
+        DESCRIPTION.
+    optimizer : torch.optim.Optimizer
+        DESCRIPTION.
+    num_epochs : int
+        DESCRIPTION.
+    device : 
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    print("Starting training")
+    for epoch in range(num_epochs):
+    # Train an epoch
+    model.train()
+    for batch_x, batch_y in dataset_train:
+        batch_x, batch_y = batch_x.to(device), batch_y.to(device)
+        
+        # Evaluate the network (forward pass)
+        model.zero_grad()
+        output = model(batch_x)
+        loss = criterion(output, batch_y)
+        
+        # Compute the gradient
+        loss.backward()
+        
+        # Update the parameters of the model with a gradient step
+        optimizer.step()
+          
+        # Test the quality on the test set
+        model.eval()
+        accuracies_test = []
+        for batch_x, batch_y in dataset_test:
+            batch_x, batch_y = batch_x.to(device), batch_y.to(device)
+            
+            # Evaluate the network (forward pass)
+            prediction = model(batch_x)
+            accuracies_test.append(metric(prediction, batch_y))
+    
+    print("Epoch {} | Test metric: {:.5f}".format(epoch, sum(accuracies_test).item()/len(accuracies_test)))
+
+def main(num_epochs = 10, learning_rate = 1e-3, batch_size = 128):  
+    
+    
+    # If a GPU is available (should be on Colab, we will use it)
+    if not torch.cuda.is_available():
+        raise Exception("Things will go much quicker if you enable a GPU in Colab under 'Runtime / Change Runtime Type'")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    criterion = torch.nn.CrossEntropyLoss() #TODO change criterion
+    model = get_model().to(device)
+    dataset_train, dataset_test = get_train_test_loaders()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    train(model, criterion, dataset_train, dataset_test, optimizer, num_epochs, device)
+    
+if __name__ =="__main__":
+    main()
+    
+    
---
+++ src/post_pipeline.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 18:57:29 2020
+
+@author: Alexander
+"""
+
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+import numpy as np
+import os 
+from PIL import Image
+import random
+
+dataFolder = "../data"
+test_filename = "DOP25_LV03_1301_11_2015_1_15_497500.0_119062.5.png"
+
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+    
+"""
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+imshow(make_grid(images[:4, : ,:, :]))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+"""
+
+def get_groudtruth_file(filename_image):
+    filename, file_extension = os.path.splitext(filename_image)
+    filename_label = filename + "_label"
+    return filename_label+file_extension
+
+
+
+def compare_labels(true_label, predicted_label):
+    array = np.zeros_like(true_label)
+    (height, width, channels) = array.shape
+    
+    true_value = np.array([255,255,255])
+    
+    true_positive = [0,255,0]
+    false_positive = [255,0,0]
+    false_negative = [255,215,0]
+    
+    for h in range(height):
+        for w in range(width):
+            if np.equal(true_label[h,w,:], predicted_label[h,w,:]).all():
+                if np.equal(true_label[h,w,:], true_value).all():
+                    array[h,w,:] = true_positive
+            else:
+                if np.equal(true_label[h,w,:], true_value).all():
+                    array[h,w,:] = false_negative
+                else:
+                    array[h,w,:] = false_positive
+    return array
+
+    
+def aa(filename):
+    img_file = os.path.join(dataFolder, "PV", filename)
+    label_filename = get_groudtruth_file(filename)
+    label_file = os.path.join(dataFolder, "PV", "labels", label_filename)
+    
+    img = Image.open(img_file)
+    label = Image.open(label_file)
+    
+    arr = np.array(img)
+    plt.imshow(arr)
+    plt.show()
+    arr_label = np.array(label)
+    print(arr_label.shape)
+    plt.imshow(arr_label)
+    plt.show()
+    # plt.imshow(img)
+
+if __name__ =="__main__":
+    filenames = random.sample(os.listdir(os.path.join(dataFolder, "PV")), 2)
+    label_files = [os.path.join(dataFolder, "PV", "labels", get_groudtruth_file(filename)) for filename in filenames]
+    labels = [np.array(Image.open(label_file)) for label_file in label_files]
+    plt.imshow(labels[0])
+    plt.show()
+    plt.imshow(labels[1])
+    plt.show()
+    true_value = np.array([1,1,1]).astype(np.uint8)
+    array = compare_labels(labels[0], labels[1])
+    plt.imshow(array)
+    plt.show()
+        
