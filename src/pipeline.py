f0f7d3c231d5d22aaf02ec0d4f05a4906a756d0f
Alexander Apostolov <alex21ander08@gmail.com>
2020-12-01 01:03:07 +0100

    model working with model with transforms

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader
 from model.unet_model import UNet
 from losses import GeneralLoss, jaccard_loss, jaccard_distance_loss, DiceLoss
 import torch.nn as nn
+from torchvision import transforms
     
 
 def train(model, criterion, dataloader_train, dataloader_test, optimizer, num_epochs, device):
@@ -26,8 +27,8 @@ def train(model, criterion, dataloader_train, dataloader_test, optimizer, num_ep
     print("Starting training")
     for epoch in range(num_epochs):
         model.train()
-        for sample_batched in dataloader_train:
-            batch_x, batch_y = sample_batched['image'], sample_batched['label']
+        for batch_x, batch_y in dataloader_train:
+            #batch_x, batch_y = sample_batched['image'], sample_batched['label']
             batch_x, batch_y = batch_x.to(device, dtype=torch.float32), batch_y.to(device, dtype=torch.float32)
                         
             # Evaluate the network (forward pass)
