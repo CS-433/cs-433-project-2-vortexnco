7e97cb820e4f78c6de1ddb9d5d05cf9aa623956c
mantheflan <auguste.apple@gmail.com>
2020-12-13 15:05:10 +0100

    Made various style changes.

--- src/AvailableRooftopDataset.py
+++ src/AvailableRooftopDataset.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils.data import Dataset
 from skimage import io
 
+
 class AvailableRooftopDataset(Dataset):
     """Available Rooftop Dataset."""
 
@@ -21,14 +22,21 @@ class AvailableRooftopDataset(Dataset):
             Directory with labels of PV files.
         transform : callable, optional
             Transform to be applied on images.
+        prop_noPV : float, optional
+            Proportion of noPV (devoid of any rooftop space) to include.
+            Must be between 0 and 1.
         """
         self.dir_PV = dir_PV
         self.dir_labels = dir_labels
         self.transform = transform
 
         # Get the list of PV_image and label names from PV and label directories (except dotfile)
-        self.images_name = [image_name for image_name in os.listdir(dir_PV) if image_name[0] != "."]        
-        self.labels_name = [label_name for label_name in os.listdir(dir_labels) if label_name[0] != "."]
+        self.images_name = [
+            image_name for image_name in os.listdir(dir_PV) if image_name[0] != "."
+        ]
+        self.labels_name = [
+            label_name for label_name in os.listdir(dir_labels) if label_name[0] != "."
+        ]
 
         # Create an image -> label dict
         self.image_label_dict = {}
@@ -45,23 +53,29 @@ class AvailableRooftopDataset(Dataset):
 
             self.image_label_dict[image_full_name] = label_name_associated
 
-        if (prop_noPV > 0.0):
+        if prop_noPV > 0.0:
             self.dir_noPV = dir_noPV
 
             # Get the list of noPV_image from the noPV directory (except dotfile)
-            self.noPV_images_name = [image_name for image_name in os.listdir(dir_noPV) if image_name[0] != "."]
+            self.noPV_images_name = [
+                image_name
+                for image_name in os.listdir(dir_noPV)
+                if image_name[0] != "."
+            ]
 
             # Keep a proportion of (random) noPV images
             torch.manual_seed(0)
             random.seed(0)
             random.shuffle(self.noPV_images_name)
-            self.noPV_images_name = self.noPV_images_name[:int(len(self.noPV_images_name) * prop_noPV)]
+            self.noPV_images_name = self.noPV_images_name[
+                : int(len(self.noPV_images_name) * prop_noPV)
+            ]
 
             # Iterate through all the noPV images' name to add them in the dict and to the images_name list
             for noPV_image_full_name in self.noPV_images_name:
                 self.image_label_dict[noPV_image_full_name] = "DEFAULT"
                 # Add a '-' before the name of the file to tag it as a noPV
-                self.images_name.append('-' + noPV_image_full_name)
+                self.images_name.append("-" + noPV_image_full_name)
 
     def __len__(self):
         return len(self.image_label_dict)
@@ -74,7 +88,7 @@ class AvailableRooftopDataset(Dataset):
         # Retrieve the image (from dir_noPV if it is tagged with '-', else from dir_PV)
         image_name = self.images_name[idx]
 
-        if (image_name[0] == '-'):
+        if image_name[0] == "-":
             # Remove the tag
             image_name = image_name[1:]
             image_path = os.path.join(self.dir_noPV, image_name)
@@ -85,12 +99,12 @@ class AvailableRooftopDataset(Dataset):
 
         # Retrieve the label if the image is from PV, else the label is full black
         label_name = self.image_label_dict[image_name]
-        
+
         if label_name != "DEFAULT":
             label_path = os.path.join(self.dir_labels, label_name)
             label = io.imread(label_path)
         else:
-            label = np.zeros((250, 250, 3), dtype=np.uint8)  
+            label = np.zeros((250, 250, 3), dtype=np.uint8)
 
         # Define a seed to apply the same transforms for 'image' and 'label'
         seed = np.random.randint(2147483647)
