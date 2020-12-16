1e9324596335c058850dd50cec8fd44982a0214d
mantheflan <auguste.apple@gmail.com>
2020-12-16 08:36:09 +0100

    Added documentation.

--- src/visualisation.py
+++ src/visualisation.py
@@ -1,7 +1,3 @@
-
-
-
-map_rgb = {0: [0, 255, 0], 1: [0, 0, 0], 2: [255, 0, 0], 3: [255, 215, 0]}
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
@@ -10,16 +6,30 @@ from PIL import Image
 from itertools import product
 
 
+map_rgb = {0: [0, 255, 0], 1: [0, 0, 0], 2: [255, 0, 0], 3: [255, 215, 0]}
 # If color values are binary
 COMPARE_MAP_01 = {(2, 0): 0, (0, 0): 1, (1, 1): 2, (1, -1): 3}
-
 # If color values are 3 bytes
 COMPARE_MAP_uint8 = {(254, 0): 0, (0, 0): 1, (255, 255): 2, (255, 1): 3}
 
 
 def compare_labels(true_label, predicted_label):
     """Outputs an array annotated as TP, FP, TN or FN as ints,
-    according to the chosen COMPARE_MAP"""
+    according to the chosen COMPARE_MAP
+
+    Inputs:
+    ========
+    true_label : ndarray
+        True label for the image; only contains 1s and 0s.
+    predicted_label : ndarray
+        Prediction from the model for the image; only contains 1s and 0s.
+
+    Returns:
+    ========
+    result : ndarray
+        Annotations indicating whether each pixel in the predicted label is a
+        true positive, true negative, false positive or false negative.
+    """
     height, width = true_label.shape
     comp_array = np.array([predicted_label + true_label, predicted_label - true_label])
     result = np.empty((height, width), dtype=int)
@@ -30,28 +40,27 @@ def compare_labels(true_label, predicted_label):
 
 def show_label_comparison(true_label, predicted_label):
     """
-    Plots an array annotated with TP, FP, TN and FN
+    Plots an array annotated with TP, FP, TN and FN.
 
-    Parameters
-    ----------
-    true_label : ndarray of 1s and 0s
-        True label.
-    predicted_label : ndarray of 1s and 0s
-        Prediction from the model.
+    Inputs:
+    ========
+    true_label : ndarray
+        True label for the image; only contains 1s and 0s.
+    predicted_label : ndarray
+        Prediction from the model for the image; only contains 1s and 0s.
 
-    Returns
-    -------
+    Returns:
+    ========
     None
     """
     comparison = compare_labels(true_label, predicted_label)
     height, width = comparison.shape
     # Convert to RGB
     comparison_rgb = np.empty((height, width, 3), dtype=int)
-    f = lambda i, j: map_rgb[comparison[i, j]]
-
     for i, j in product(range(height), range(width)):
         comparison_rgb[i, j, :] = map_rgb[comparison[i, j]]
 
+    # Plot the array
     plt.imshow(comparison_rgb)
     TP = mpatches.Patch(color="green", label="TP")
     TN = mpatches.Patch(color="black", label="TN")
@@ -114,26 +123,26 @@ def plot_precision_recall_f1(
 
     plt.figure(2)
     ax_prec_rec = plt.axes()
-    ax_prec_rec.fill_between(rec_mid, prec_lower, prec_upper, alpha=0.6)
-    ax_prec_rec.plot(rec_mid, prec_mid)
+    ax_prec_rec.fill_between(recall_mid, precision_lower, precision_upper, alpha=0.6)
+    ax_prec_rec.plot(recall_mid, precision_mid)
     # Should I use the lower and upper rec??
     # What's the significance of this statistically?
-    # ax_prec_rec.plot(rec_lower, prec_lower)
-    # ax_prec_rec.plot(rec_upper, prec_upper)
-    # ax_prec_rec.fill_between(rec_lower, prec_lower, prec_mid, alpha=0.6)
-    # ax_prec_rec.fill_between(rec_upper, prec_mid, prec_upper, alpha=0.6)
+    # ax_prec_rec.plot(recall_lower, precision_lower)
+    # ax_prec_rec.plot(recall_upper, precision_upper)
+    # ax_prec_rec.fill_between(recall_lower, precision_lower, precision_mid, alpha=0.6)
+    # ax_prec_rec.fill_between(recall_upper, precision_mid, precision_upper, alpha=0.6)
     plt.grid(True)
     plt.xlabel("Recall")
     plt.ylabel("Precision")
     if idx_best:
         # Annotate with best estimator
-        rec_best = rec_mid[idx_best]
-        prec_best = prec_mid[idx_best]
-        ax_prec_rec.plot([rec_best], [prec_best], "ro")
+        recall_best = recall_mid[idx_best]
+        precision_best = precision_mid[idx_best]
+        ax_prec_rec.plot([recall_best], [precision_best], "ro")
         plt.text(
-            rec_best + 0.02,
-            prec_best + 0.02,
-            "({:.3f}, {:.3f})".format(rec_best, prec_best),
+            recall_best + 0.02,
+            precision_best + 0.02,
+            "({:.3f}, {:.3f})".format(recall_best, precision_best),
         )
 
     plt.show()
